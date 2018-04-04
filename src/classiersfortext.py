bca8174e2052d7dde50b8a4b136717c54f7d9834
Aribido-Oluwaseun <oja@gatech.edu>
2018-04-04 16:47:53 -0400

    Adding UI Folder

--- src/classiersfortext.py
+++ src/classiersfortext.py
@@ -5,7 +5,13 @@ from nltk.tokenize import RegexpTokenizer
 from sklearn.svm import SVC
 from sklearn.multiclass import OneVsOneClassifier
 from sklearn.linear_model import LogisticRegression
-from nltk.stem.lancaster import LancasterStemmer
+from nltk import pos_tag
+from nltk.corpus import wordnet as wn
+from warnings import warn
+import copy
+import json
+
+
 
 class TrainTieBotException(Exception):
     "Defines error of logistic reg learner"
@@ -13,6 +19,7 @@ class TrainTieBotException(Exception):
 
 class CorpusException(Exception):
     "Defines error for the Corpus class"
+
 
 class NeuralNetException(Exception):
     "Defined NeuralNet Exceptions"
@@ -50,6 +57,62 @@ class Corpus:
     def getClasses(self):
         return list(set([int(x.keys()[0]) for x in self.corpus]))
 
+    def getSimilarWords(self, sentence, pos='NN'):
+        ttb = TrainTieBot()
+        similarWords = {}
+        tokenDict = ttb.tokenize(sentence)
+        wordFeatures = ttb.getFeaturesByName(tokenDict.values())
+        for eachTuple in pos_tag(wordFeatures):
+            if eachTuple[1].startswith(pos):
+                for i, j in enumerate(wn.synsets(eachTuple[0])):
+                    similarWords[str(eachTuple[0])] = [str(x) for x in j.lemma_names()]
+                return similarWords, tokenDict.values()[0]
+
+    def generateSimilarSentences(self, sentence, pos='NN', num_of_sentences=5):
+        """ This function generates similar sentences using the synonyms
+        of word depending on the part of speech passed
+        The sentence argument must be a dict object.
+        """
+        assert (isinstance(sentence, dict))
+        originalSentence = copy.deepcopy(sentence.values()[0])
+        sentenceClass = sentence.keys()[0]
+        newSentence = copy.deepcopy(sentence)
+        similarWords = None
+        tokens = None
+        try:
+            similarWords, tokens = self.getSimilarWords(newSentence, pos)
+        except (TypeError) as err:
+            print('The Part of speech {} is not available'.format(pos))
+            return None
+        # Note that sentence becomes a string here
+        sentences = list()
+        word_indices = dict()
+        temp = num_of_sentences
+
+        for word, values in similarWords.iteritems():
+            num_of_sentences = np.min([num_of_sentences, len(values)])
+            word_indices[word] = tokens.index(word)
+
+        if temp < num_of_sentences:
+            warn('Number of available sentences is less than %s', temp)
+        for eachWord in similarWords:
+            count = 0
+            while (count < num_of_sentences):
+                sentence = tokens
+                sentence[word_indices[eachWord]] = str(similarWords[eachWord][count])
+                someSentence = ' '.join(sentence)
+                sentences.append({sentenceClass: someSentence})
+                count += 1
+        return sentences
+
+    def getExpandedSentences(self, corpus):
+        sentences = []
+        for eachWord in corpus:
+            x = self.generateSimilarSentences(eachWord)
+            if x != []:
+                sentences.extend(x)
+
+        return sentences
 
 class SVMClassifier:
 
@@ -98,88 +161,83 @@ class NeuralNet:
     def sigmoidDerv(self, x):
         return x*(1-x)
 
-    def train(self, X, y, hidden_neurons=10, alpha=1, epochs=5000, dropout=False, dropout_percent=0.5):
-
+    def train(self, X, y, hidden_neurons=50, alpha=1, epochs=10000, dropout=False, dropout_percent=0.5):
         print ("Training with %s neurons, alpha:%s, dropout:%s %s" % (
         hidden_neurons, str(alpha), dropout, dropout_percent if dropout else ''))
         print ("Input matrix: %sx%s    Output matrix: %sx%s" % (len(X), len(X[0]), 1, len(0)))
         np.random.seed(1)
-
+        output = np.zeros([1, len(y)])
         last_mean_error = 1
         # randomly initialize our weights with mean 0
-        synapse_0 = 2 * np.random.random((len(X[0]), hidden_neurons)) - 1
-        synapse_1 = 2 * np.random.random((hidden_neurons, len(0))) - 1
+        W_0 = 2 * np.random.random((len(X[0]), hidden_neurons)) - 1
+        W_1 = 2 * np.random.random((hidden_neurons, len(output))) - 1
 
-        prev_synapse_0_weight_update = np.zeros_like(synapse_0)
-        prev_synapse_1_weight_update = np.zeros_like(synapse_1)
+        prev_W_0_weight_update = np.zeros_like(W_0)
+        prev_W_1_weight_update = np.zeros_like(W_1)
 
-        synapse_0_direction_count = np.zeros_like(synapse_0)
-        synapse_1_direction_count = np.zeros_like(synapse_1)
-        """
+        W_0_direction_count = np.zeros_like(W_0)
+        W_1_direction_count = np.zeros_like(W_1)
+
         for j in iter(range(epochs + 1)):
 
             # Feed forward through layers 0, 1, and 2
-            layer_0 = X
-            layer_1 = sigmoid(np.dot(layer_0, synapse_0))
+            l_0 = X
+            l_1 = self.sigmoid(np.dot(l_0, W_0))
 
             if (dropout):
-                layer_1 *= np.random.binomial([np.ones((len(X), hidden_neurons))], 1 - dropout_percent)[0] * (
+                l_1 *= np.random.binomial([np.ones((len(X), hidden_neurons))], 1 - dropout_percent)[0] * (
                             1.0 / (1 - dropout_percent))
 
-            layer_2 = sigmoid(np.dot(layer_1, synapse_1))
+            l_2 = self.sigmoid(np.dot(l_1, W_1))
 
             # how much did we miss the target value?
-            layer_2_error = y - layer_2
+            l_2_error = y - l_2
 
-            if (j % 10000) == 0 and j > 5000:
-                # if this 10k iteration's error is greater than the last iteration, break out
-                if np.mean(np.abs(layer_2_error)) < last_mean_error:
-                    print ("delta after " + str(j) + " iterations:" + str(np.mean(np.abs(layer_2_error))))
-                    last_mean_error = np.mean(np.abs(layer_2_error))
+            if (j % int(epochs/5)) == 0 and j > epochs/10:
+                # if this zzz iteration's error is greater than the last iteration, break out
+                if np.mean(np.abs(l_2_error)) < last_mean_error:
+                    print ("delta after " + str(j) + " iterations:" + str(np.mean(np.abs(l_2_error))))
+                    last_mean_error = np.mean(np.abs(l_2_error))
                 else:
-                    print ("break:", np.mean(np.abs(layer_2_error)), ">", last_mean_error)
+                    print ("break:", np.mean(np.abs(l_2_error)), ">", last_mean_error)
                     break
 
             # in what direction is the target value?
             # were we really sure? if so, don't change too much.
-            layer_2_delta = layer_2_error * sigmoid_output_to_derivative(layer_2)
+            layer_2_delta = l_2_error * self.sigmoidDerv(l_2)
 
             # how much did each l1 value contribute to the l2 error (according to the weights)?
-            layer_1_error = layer_2_delta.dot(synapse_1.T)
+            layer_1_error = layer_2_delta.dot(W_1.T)
 
             # in what direction is the target l1?
             # were we really sure? if so, don't change too much.
-            layer_1_delta = layer_1_error * sigmoid_output_to_derivative(layer_1)
+            layer_1_delta = layer_1_error * self.sigmoidDerv(l_1)
 
-            synapse_1_weight_update = (layer_1.T.dot(layer_2_delta))
-            synapse_0_weight_update = (layer_0.T.dot(layer_1_delta))
+            W_1_weight_update = (l_1.T.dot(layer_2_delta))
+            W_0_weight_update = (l_0.T.dot(layer_1_delta))
 
             if (j > 0):
-                synapse_0_direction_count += np.abs(
-                    ((synapse_0_weight_update > 0) + 0) - ((prev_synapse_0_weight_update > 0) + 0))
-                synapse_1_direction_count += np.abs(
-                    ((synapse_1_weight_update > 0) + 0) - ((prev_synapse_1_weight_update > 0) + 0))
+                W_0_direction_count += np.abs(
+                    ((W_0_weight_update > 0) + 0) - ((prev_W_0_weight_update > 0) + 0))
+                W_1_direction_count += np.abs(
+                    ((W_1_weight_update > 0) + 0) - ((prev_W_1_weight_update > 0) + 0))
 
-            synapse_1 += alpha * synapse_1_weight_update
-            synapse_0 += alpha * synapse_0_weight_update
+            W_1 += alpha * W_1_weight_update
+            W_0 += alpha * W_0_weight_update
 
-            prev_synapse_0_weight_update = synapse_0_weight_update
-            prev_synapse_1_weight_update = synapse_1_weight_update
-
-        now = datetime.datetime.now()
+            prev_W_0_weight_update = W_0_weight_update
+            prev_W_1_weight_update = W_1_weight_update
 
         # persist synapses
-        synapse = {'synapse0': synapse_0.tolist(), 'synapse1': synapse_1.tolist(),
-                   'datetime': now.strftime("%Y-%m-%d %H:%M"),
-                   'words': words,
-                   'classes': classes
+        synapse = {'W0': W_0.tolist(), 'W1': W_1.tolist(),
+                   'classes': output
                    }
         synapse_file = "synapses.json"
 
         with open(synapse_file, 'w') as outfile:
             json.dump(synapse, outfile, indent=4, sort_keys=True)
         print ("saved synapses to:", synapse_file)
-        """
+
 
 class TrainTieBot:
     def __init__(self):
@@ -206,7 +264,7 @@ class TrainTieBot:
             raise(TrainTieBotException('Corpus must be of type:list()'))
         newCorpus = list()
         for eachList in corpus:
-            newCorpus.extend(eachList.values()[0])
+            newCorpus.extend(eachList)
         corpus = newCorpus
         self.wordFeatures = list(set(corpus))
         return self.wordFeatures

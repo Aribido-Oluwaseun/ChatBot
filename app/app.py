37e6335f29f3b1eb8c1f8fa6600af367de9ad86b
Aribido-Oluwaseun <oja@gatech.edu>
2018-04-21 23:19:45 -0400

    added capability for LR and SVM

--- app/app.py
+++ app/app.py
@@ -1,5 +1,4 @@
 from flask import Flask, render_template
-from flask import session
 from flask import request
 import classiersfortext
 
@@ -8,31 +7,41 @@ app = Flask(__name__)
 classifier = None
 tiebot = None
 
+
 @app.route('/')
 def start_bot():
-	global classifier
-	global tiebot
-	classifier, tiebot = classiersfortext.train()
-	return render_template('home.html')
+    global classifier
+    global tiebot
+    global classifier_type
+    classifier_type = 'DNN'
+    classifier, tiebot = classiersfortext.train(classifier_type)
+    return render_template('home.html')
+
 
 @app.route('/question', methods=['POST', 'GET'])
 def print_something():
-	global answer
-	if request.method == 'POST':
-		x=request.form['question']
-		answer=classiersfortext.predict(classifier=classifier, tiebot=tiebot, question=x)
-		return render_template('home.html')
-	if request.method == 'GET':
-		return answer
-	
+    global answer
+    if request.method == 'POST':
+        x = request.form['question']
+        answer = classiersfortext.predict(classifier=classifier,
+                                          tiebot=tiebot,
+                                          question=x,
+                                          classifier_type=classifier_type)
+        return render_template('home.html')
+    if request.method == 'GET':
+        return answer
+
+
 @app.route('/feedback', methods=['POST', 'GET'])
 def send_feedback():
-	if request.method == 'POST':
-		question = request.form['question']
-		answer = request.form['feedback']
-		checkFeedbadk = classiersfortext.takeFeedBack(question, answer)
-	if request.method == 'GET':
-		return render_template('home.html')
+    if request.method == 'POST':
+        question = request.form['question']
+        answer = request.form['feedback']
+        checkFeedbadk = classiersfortext.takeFeedBack(question, answer)
+        print 'Feedback ackknoledged'
+    if request.method == 'GET':
+        return render_template('home.html')
+
 
 if __name__=='__main__':
     app.run()
